4d5266eb0b5acabde93b8a85b0540e523235d8f5
Jose Reyes Palacio <jrpalacio.dev@gmail.com>
2025-12-21 17:30:12 -0600

    refactor(main.ts): Simplifique el proceso de arranque y elimine el código de inicialización de base de datos no utilizado

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,20 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
-import { PGlite } from '@electric-sql/pglite'
-import { live } from '@electric-sql/pglite/live'
-import schema from '@/db/schemas.sql?raw'
-
 import App from './App.vue'
 import router from './router'
-import { vector } from '@electric-sql/pglite/vector'
+import { initDB } from '@/db/init'
 
-const app = createApp(App)
+async function bootstrap() {
+  const app = createApp(App)
 
-const db = new PGlite('idb://pglite-tickets', {
-  extensions: {
-    live,
-    vector,
-  },
-})
+  const db = await initDB()
+  app.provide('db', db)
 
-// Usar provide nativo de Vue
-app.provide('db', db)
+  app.use(createPinia())
+  app.use(router)
 
-// Consulta inicial para verificar la tabla de productos
-const result = await db.query('SELECT * FROM productos LIMIT 1')
-if (result.rows.length > 0) {
-  console.log('Productos existentes:', result.rows)
-} else {
-  // Ejecuta el schema SQL (usa IF NOT EXISTS para evitar errores)
-  console.log('Ejecutando schema SQL...')
-  await db.exec(schema)
-  console.log('Base de datos lista!')
+  app.mount('#app')
 }
 
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+bootstrap()
