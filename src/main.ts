46e469f20a649bba2d031494b6c055b5402ac5b2
Jose Reyes Palacio <jrpalacio.dev@gmail.com>
2025-12-21 15:44:08 -0600

    Add vector extension to PGlite and adjust initial product query

--- src/main.ts
+++ src/main.ts
@@ -10,16 +10,21 @@ import schema from '@/db/schemas.sql?raw'
 
 import App from './App.vue'
 import router from './router'
+import { vector } from '@electric-sql/pglite/vector'
 
 const app = createApp(App)
 
 const db = new PGlite('idb://pglite-tickets', {
   extensions: {
     live,
+    vector,
   },
 })
+// @ts-expect-error - PGlite with live extension is compatible but types don't match exactly
+providePGlite(db)
+
 // Consulta inicial para verificar la tabla de productos
-const result = await db.query(`SELECT * FROM productos;`)
+const result = await db.query('SELECT * FROM productos LIMIT 1')
 if (result.rows.length > 0) {
   console.log('Productos existentes:', result.rows)
 } else {
@@ -28,9 +33,6 @@ if (result.rows.length > 0) {
   await db.exec(schema)
   console.log('Base de datos lista!')
 }
-
-// @ts-expect-error - PGlite with live extension is compatible but types don't match exactly
-providePGlite(db)
 
 app.use(createPinia())
 app.use(router)
