591c975d65fd1c0ef1ba8a598f05fbdd62565d10
Jose Reyes Palacio <jrpalacio.dev@gmail.com>
2025-12-22 18:05:28 -0600

    refactor(HomeView): Remove redundant header and replace delete button text with an icon

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -63,9 +63,6 @@ onMounted(async () => {
 </script>
 
 <template>
-  <header>
-    <h1>Bienvenido al Sistema de Tickets</h1>
-  </header>
   <main>
     <div v-if="loading" class="loading">Cargando productos...</div>
     <div v-else class="tickets-container">
@@ -83,7 +80,22 @@ onMounted(async () => {
               </option>
             </select>
             <button @click="eliminarSelect(index)" class="btn-eliminar" type="button">
-              Eliminar
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-trash-icon lucide-trash"
+              >
+                <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" />
+                <path d="M3 6h18" />
+                <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+              </svg>
             </button>
           </div>
         </div>
